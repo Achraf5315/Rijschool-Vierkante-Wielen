173be8f3ba80b27f917c5936b7766457535d38e3
Achraf <338127@student.mboutrecht.nl>
2026-05-28 11:14:00 +0200

    Pagina aangemaakt + sql script

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('payment.index')" :active="request()->routeIs('payment.index')">
+                        {{ __('Betalingen') }}
+                    </x-nav-link>
                 </div>
             </div>
 
