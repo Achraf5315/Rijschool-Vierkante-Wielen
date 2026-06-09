9e43101e9f638f16016d48a484f94471f44da2e0
Achraf <338127@student.mboutrecht.nl>
2026-06-09 11:12:42 +0200

    dev fixen

--- resources/views/components/application-logo.blade.php
+++
@@ -1,5 +0,0 @@
-@if (in_array(Route::currentRouteName(), ['dashboard', 'management-dashboard', 'facturatie.index', 'reis.index', 'welcome', 'ticket.index', 'boekingen.index', 'reis.show', 'ticket.show', 'accommodatie.index', 'facturatie.bewerken']))
-    <img src="{{ asset('img/traveleasy-logo-dashboard.png') }}" alt="TravelEasy-logo" class="logo-dashboard" />
-@else
-    <img src="{{ asset('img/traveleasy-logo.png') }}" alt="TravelEasy-logo" class="logo-klein" />
-@endif
